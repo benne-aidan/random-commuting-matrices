69d7d89e12f2dd644790f6ed2b1d5693fb3ecbb3
benne-aidan <bennettaidan03@gmail.com>
2024-04-15 21:59:22 -0400

    Testing eigenvalue computation

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <chrono>
 #include <Eigen/Dense>
 #include <EigenRand/EigenRand>
  
@@ -7,15 +8,26 @@ using namespace Eigen;
 int main() {
     // Initialize random number generator with seed=42 for following codes.
     // Or you can use C++11 RNG such as std::mt19937 or std::ranlux48.
-    Rand::P8_mt19937_64 urng{ 42 };
-    
-    // this will generate 4x4 real matrix with range [-1, 1]
-    MatrixXi mat = Rand::uniformInt<MatrixXi>(4, 4, urng, 1, 10);
+    EigenSolver<MatrixXd> es;
+
+    //unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    //Rand::P8_mt19937_64 urng{seed}; 
+    //
+    //MatrixXd mat = Rand::normal<MatrixXd>(10, 10, urng);
+    //std::cout << mat << std::endl;
+
+    MatrixXd mat(2, 2);
+    mat(0,0) = 1;
+    mat(0,1) = 2;
+    mat(1,0) = 3;
+    mat(1,1) = 4;
+
+
     std::cout << mat << std::endl;
-    
-    // this will generate 10x10 real 2d array on the normal distribution
-    ArrayXXf arr = Rand::normal<ArrayXXf>(10, 10, urng);
-    std::cout << arr << std::endl;
+
+    es.compute(mat, false);
+
+    std::cout << "Eigenvalues are: " << es.eigenvalues().transpose() << std::endl;
     
     return 0;
 }
